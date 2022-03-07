22074e612770a46a1c60a7c0f7da70ce7a92bb83
Emileco <emileco4@gmail.com>
2022-03-07 17:35:48 -0600

    Funcion contar

--- src/Proyecto/ManejoDatos.java
+++ src/Proyecto/ManejoDatos.java
@@ -21,4 +21,46 @@ public class ManejoDatos {
        }
        return list;
     }
+
+
+public static ArrayList<String> quitarcasos(ArrayList<String> list)
+{
+    ArrayList <String> list2 = new ArrayList <String>();
+    int tamanio;
+    String aux;
+    tamanio=list.size();
+
+    for (int i = 0; i < tamanio; i++) {
+        aux=(list.get(i).replaceAll("\\d",""));
+        aux=(aux.replaceAll("\\W"," "));
+        list2.add(aux);
+    }
+        return list2;
+    
+}
+
+public static ArrayList<Integer> quitarPalabras(ArrayList<String> list,ArrayList<String> list2){
+        ArrayList <Integer> listnum = new ArrayList <Integer>();
+        int tamanio,tamanio2=0,contador=0;
+        tamanio2=list2.size();
+        int[] contadores = new int[tamanio2];
+        String aux;
+      //  list2=regresaArrayList(ruta); 
+        tamanio=list.size();
+        tamanio2=list2.size();
+        for (int i = 0; i < tamanio; i++) {
+            aux=list.get(i);
+           for (int j = 0; j < tamanio2; j++) {
+               if(list.get(i).contains(list2.get(j))==true){
+                   contadores[j]+=1;
+               }
+           } 
+        }
+        for (int i = 0; i < tamanio2; i++) {
+            listnum.add(contadores[i]);
+        }
+
+        return listnum;
+        
+    }
 }
--- src/Proyecto/Principal.java
+++ src/Proyecto/Principal.java
@@ -16,14 +16,24 @@ public class Principal {
 public static void main(String[] args) {
 // TODO Auto-generated method stub
 
-int tamaño, cont = 0 , i;
+int tamaño, i=0;
 
 
 
 ArrayList <String> list = new ArrayList <String>();
 ArrayList <String> list2 = new ArrayList <String>();
+ArrayList <Integer> cont = new ArrayList <Integer>();
 list = ManejoDatos.regresaArrayList("/home/ic20elp/Downloads/tweets.csv");
+list2 = ManejoDatos.regresaArrayList("/home/ic20elp/POO/Practica1/Practica1/src/Proyecto/Prueba.txt");
+list = ManejoDatos.quitarcasos(list);
+cont = ManejoDatos.quitarPalabras(list, list2);
 for(String txt: list)
 System.out.println(txt);
+
+for(Integer num: cont)
+{
+System.out.println(""+list2.get(i)+" : "+num);
+i++;
+}
 }
 }
