6210407e42ce4e99bcfa371ba536f966d63d3b6a
Emileco <emileco4@gmail.com>
2022-03-07 16:59:36 -0600

    Funcion quitar

--- ManejoDatos.java
+++ ManejoDatos.java
@@ -21,4 +21,21 @@ public class ManejoDatos {
        }
        return list;
     }
+
+
+public static ArrayList<String> quitarcasos(ArrayList<String> list)
+{
+    ArrayList <String> list2 = new ArrayList <String>();
+    int tamanio;
+    String aux;
+    tamanio=list.size();
+
+    for (int i = 0; i < tamanio; i++) {
+        aux=(list.get(i).replaceAll("\\d",""));
+        aux=(aux.replaceAll("\\W"," "));
+        list2.add(aux);
+    }
+        return list2;
+    
+}
 }
--- Principal.java
+++ Principal.java
@@ -21,8 +21,8 @@ int tamaño, cont = 0 , i;
 
 
 ArrayList <String> list = new ArrayList <String>();
-ArrayList <String> list2 = new ArrayList <String>();
 list = ManejoDatos.regresaArrayList("/home/ic20elp/Downloads/tweets.csv");
+list = ManejoDatos.quitarcasos(list);
 for(String txt: list)
 System.out.println(txt);
 }
